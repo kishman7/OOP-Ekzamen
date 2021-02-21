5d01d7cd888f9b35a947c27b421276af3f2de74b
kishman7 <kishman_us@ukr.net>
2021-02-21 23:11:55 +0200

    Add Ekzamen OOP 2

--- Fortress/Fortress.h
+++ Fortress/Fortress.h
@@ -13,26 +13,65 @@ using namespace std;
 class Shooter {
 protected:
 	string amunition;
-	int num_amunition;
+	int* num_amunition;
 public:
-	void AddAmunition(string amunition, int num_amunition) { // метод додавання амуниції
-
+	void AddAmunition(string shooter, string amunition, int* num_amunition) { // метод додавання амуниції
 		this->amunition = amunition;
 		this->num_amunition = num_amunition;
-		cout << "Ammunition: " << this->amunition << "Amount ammunition: " << this->num_amunition << endl;
-
+		cout<<"Ammunition "<< shooter << ". Name ammunition: " << this->amunition << ". Amount ammunition: " << *(this->num_amunition) << endl;
 	}
 	void Shot() { // метод постріл
-		this->num_amunition--;
-		cout << "Shot done!!! Amount of " << this->amunition << ": " << this->num_amunition << endl;
+		if (*num_amunition <= 0)
+		{
+			cout << "No ammunition" << endl;
+		}
+		else
+		{
+			*num_amunition = *(this->num_amunition) - 1; // значення віднмає 1, аналог num_amunition--
+			cout << "Shot done!!! Amount of " << this->amunition << ": " << *(this->num_amunition) << endl;
+		}
 	}
+		
 };
 
 class Archer : public Shooter {
 public:
-	int arrow;
-	void Shot() { // метод постріл
-		this->num_amunition--;
-		cout << "Shot done!!! Amount of " << this->amunition << ": " << this->num_amunition << endl;
+	int arrow; // стріли
+	void Shot() 	{ // метод постріл
+		cout << "Archer makes a shot." << endl;
+		((Shooter*)this)->Shot();
 	}
 };
+
+class Ranger : public Shooter {
+public:
+	int arrow;
+	void Shot() { // метод постріл
+		cout << "Ranger makes a shot." << endl;
+		((Shooter*)this)->Shot();
+	}
+};
+class Catapult : public Shooter {
+public:
+	int stone;
+	void Shot() { // метод постріл
+		cout << "Catapult makes a shot." << endl;
+		((Shooter*)this)->Shot();
+	}
+};
+class ArcherTower : public Archer, public Ranger {
+public:
+	ArcherTower(int* a) : Archer(), Ranger() {
+		((Shooter*)((Archer*)this))->AddAmunition("Archer", "arrows",a);
+		((Shooter*)((Ranger*)this))->AddAmunition("Ranger", "arrows", a);
+	}
+};
+
+class Fortress : public ArcherTower, public Catapult {
+public:
+	static Fortress* obj;
+	static Fortress* getObj(int* arrows, int* stones);
+	Fortress(int* arrows, int* stones) : ArcherTower(arrows), Catapult(){
+		((Shooter*)((Catapult*)this))->AddAmunition("Catapult", "stones", stones);
+	}
+ };
--- Fortress/Main.cpp
+++ Fortress/Main.cpp
@@ -1,18 +1,32 @@
 #include "Fortress.h"
 
-int main() {
-	Shooter shooter;
-	string amunition;
-	int num_amunition;
-	cout << "Enter name ammunition: ";
-	cin >> amunition;
-	cout << "Enter amount  ammunition: ";
-	cin >> num_amunition;
-	shooter.AddAmunition(amunition, num_amunition);
-	system("pause");
-	shooter.Shot();
+Fortress* Fortress::getObj(int* arrows, int* stones)
+{
+	if (obj == nullptr)
+	{
+		obj = new Fortress(arrows, stones);
+	}
+	return obj;
+}
 
-	
+int main() {
+	int arrow = 4;
+	int stone = 1;
+
+	Fortress* fortress = Fortress::getObj(&arrow, &stone);
+	//Fortress fortress(&arrow, &stone);
+	//Fortress fortress1(&arrow, &stone);
+
+	((Archer*)((ArcherTower*)&fortress))->Shot();
+	((Archer*)((ArcherTower*)&fortress))->Shot();
+	((Archer*)((ArcherTower*)&fortress))->Shot();
+
+	((Ranger*)((ArcherTower*)&fortress))->Shot();
+	((Ranger*)((ArcherTower*)&fortress))->Shot();
+	((Catapult*)(&fortress))->Shot();
+
+	//((Archer*)((ArcherTower*)&fortress1))->Shot();
+
 
 	return 0;
 }
